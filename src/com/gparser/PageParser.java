d79b6b98a7dac8edb9bc3d38f019d6fb5675d870
CrazzzyE1 <litvakivan@gmail.com>
2020-02-05 22:04:09 +0300

    12

--- src/com/gparser/PageParser.java
+++ src/com/gparser/PageParser.java
@@ -1,4 +1,26 @@
 package com.gparser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class PageParser {
+
+    String url;
+
+    public PageParser(String url) {
+        this.url = url;
+    }
+
+
+
+    public void run() throws IOException {
+
+        Document doc = Jsoup.connect(url).timeout(10*1000).get(); ;
+
+    }
+
 }
