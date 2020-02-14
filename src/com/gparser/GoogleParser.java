93f1393e5a571538f18bfac6b970fcf3bcd06bc4
CrazzzyE1 <litvakivan@gmail.com>
2020-02-14 18:24:09 +0300

    Create mixQueries cities + queries mixSearchGooglelinks

--- src/com/gparser/GoogleParser.java
+++ src/com/gparser/GoogleParser.java
@@ -9,25 +9,19 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class GoogleParser {
-    String query;
+    ArrayList<String> queries;
     String searchLine;
-    String nextPageLink = "OLOLOLOL";
-    int counter = 0;
 
-    public GoogleParser(String query) {
-        this.query = query;
+    public GoogleParser(ArrayList<String> queries) {
+        this.queries = queries;
     }
 
     public ArrayList<GoogleSearchPage> run() throws IOException {
-        searchLine = "https://www.google.com/search?q=" + query + "&num=500";
         ArrayList<GoogleSearchPage> gsps = new ArrayList<>();
-
-//        while (!nextPageLink.isEmpty() && counter <= 1) {
-//            System.setProperty("http.proxyHost", "77.244.42.178");
-//            System.setProperty("http.proxyPort", "4145");
-            Document doc = Jsoup.connect(searchLine).timeout(10*1000).get(); ;
+        for (int i = 0; i < queries.size(); i++) {
+            searchLine = "https://www.google.com/search?q=" + queries.get(i) + "&num=500";
+            Document doc = Jsoup.connect(searchLine).timeout(50*1000).get(); ;
             Elements links = doc.select("div.r");
-            Elements nextPageLinks = doc.getElementsByAttributeValue("class", "fl");
             links.forEach(link -> {
                 Element a = link.child(0);
                 String url = a.attr("href");
@@ -35,15 +29,7 @@ public class GoogleParser {
                 gsps.add(new GoogleSearchPage(title, url));
             });
 
-            nextPageLinks.forEach(np -> {
-                nextPageLink = np.attr("href");
-                searchLine = "https://www.google.com" + nextPageLink;
-//                System.out.println(searchLine);
-
-            });
-            counter++;
-//        }
-
+        }
 
         return gsps;
     }
